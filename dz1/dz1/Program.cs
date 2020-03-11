a968f791b5d5fdcf145e5aa097e074283b40dbda
gomulin.danila <gomulin.danila2012@yandex.ru>
2020-03-11 19:48:09 +0300

    init commit

---
+++ dz1/dz1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+
+            Console.Write("Общее количество элементов в стеке : ");
+            Console.WriteLine(myStack.Count);
+
+            myStack.Push("first");
+            myStack.Push("second");
+            myStack.Push("third");
+            myStack.Push("fourth");
+
+            Console.Write("Добавил : ");
+            Console.Write(myStack.Count);
+
+
+            Console.Write("убрал верхний элемент из стека : ");
+            Console.WriteLine(myStack.Pop());
+
+            Console.Write("Стек пуст? : ");
+            if (myStack.Count == 0)
+            {
+                Console.Write(true);
+            }
+            else
+            {
+                Console.Write(false);
+            }
+
+            Console.WriteLine();
+            Console.Write("Общее количество элементов в стеке : ");
+            Console.WriteLine(myStack.Count);
+
+            Console.Write("Вернул элемент сверху стека : ");
+            Console.WriteLine(myStack.Peek());
+
+            Console.ReadKey();
+        }
+    }
+}
